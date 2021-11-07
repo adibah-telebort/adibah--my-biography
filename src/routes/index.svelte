e20f5b81ed9ab7ffd9710d11fe45a5addd2d2091
adibah-telebort <adibah.telebort@gmail.com>
2021-11-07 08:07:03 +0000

    start

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>import Header fr
+    om '$lib/Header.svelte';
+    import Section from '$lib/Section.svelte';
+
+    let myInfo = {
+        title: "Info",
+        item1: "Adibah",
+        item2: 18,
+        item3: "Telebort",
+    }
+
+    let myHobby = {
+        title: "Hobby",
+        item1: "Play games",
+        item2: "Swimming",
+        item3: "Play badminton",
+    }
+
+</script>
+
+<Header title="My Biography" />
+<Section {...myInfo}/>
